657b2168074dcce9a6f7492ac1a5946e124c949a
Hugo Laquerre <Laquerre.hugo@gmail.com>
2015-09-25 23:09:49 -0400

    CrosswordGrid, fixes #2
    finished TwoDimensionalArray,
    created CrosswordGrid,
    finished importing from old project.

---
+++ Crossword.Builder/CrosswordGrid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossword.Builder
+{
+    class CrosswordGrid
+    {
+        #region Properties
+        private TwoDimensionArray<Char> _grid;
+
+        public readonly Dictionary<Word.Placement, Word> Words;
+        public readonly List<Point> Blanks;
+
+        public readonly char Blank;
+        #endregion
+
+        public CrosswordGrid(int columnCount, int rowCount, char blank)
+        {
+            Blank = blank;
+
+            _grid = new TwoDimensionArray<char>(columnCount, rowCount);
+
+            Words = new Dictionary<Word.Placement, Word>();
+            Blanks = new List<Point>();
+        }
+
+        private void Insert(Word word, Word.Placement placement) =>
+            _grid.Insert(word.Value.ToCharArray(), placement.Column, placement.Row, placement.IsHorizontal);
+
+    }
+}
--- Crossword.Builder/TwoDimensionArray.cs
+++ Crossword.Builder/TwoDimensionArray.cs
@@ -31,21 +31,24 @@ namespace Crossword.Builder
         }
         #endregion
 
+        public T this[int column, int row] => _array[column, row];
+
         public Boolean IsInGrid(int column, int row) =>
             (column >= 0 && column < ColumnCount) //Column
             && (row >= 0 && row < RowCount); //Row
 
+        //Todo: test Insert
         public void Insert(T[] elements, int column, int row, bool isHorizontal = true)
         {
             if (!IsInGrid(column, row))
-                throw new Exception("Outside of the grid, during the insert"); //Todo: TwoDimensionalArray.Insert throw execption
+                throw new IndexOutOfRangeException("Outside of the grid, during the insert"); //Todo: TwoDimensionalArray.Insert throw execption
 
             if (isHorizontal)
             {
                 //Insert Honrizontally
 
                 if (!IsInGrid(column + elements.Length - 1, row))
-                    throw new Exception("Outside of the grid, during the insert"); //Todo: TwoDimensionalArray.Insert throw execption
+                    throw new IndexOutOfRangeException("Outside of the grid, during the insert"); //Todo: TwoDimensionalArray.Insert throw execption
 
                 for (int i = 0; i < elements.Length; i++)
                     _array[column + i, row] = elements[i];
@@ -55,7 +58,7 @@ namespace Crossword.Builder
                 //Insert Vertically
 
                 if (!IsInGrid(column, row + elements.Length - 1))
-                    throw new Exception("Outside of the grid, during the insert"); //Todo: TwoDimensionalArray.Insert throw execption
+                    throw new IndexOutOfRangeException("Outside of the grid, during the insert"); //Todo: TwoDimensionalArray.Insert throw execption
 
                 for (int i = 0; i < elements.Length; i++)
                     _array[column, row + i] = elements[i];
@@ -65,7 +68,7 @@ namespace Crossword.Builder
         public void Insert(T element, int column, int row)
         {
             if (!IsInGrid(column, row))
-                throw new Exception("Outside of the grid, during the Insert"); //Todo: TwoDimensionalArray.Insert throw execption
+                throw new IndexOutOfRangeException("Outside of the grid, during the Insert"); //Todo: TwoDimensionalArray.Insert throw execption
 
             _array[column, row] = element;
         }
